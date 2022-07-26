cdd6db8f1377da2cbfa697ab5987b403206e9672
isaac <isaacd4444@gmail.com>
2022-07-25 21:03:23 -0400

    horizontal win checker'

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //gameboard object
 const gameBoard=(()=>{
-    const board=["x","o","x","o","x","o","x","o","x"];
+    const board=["x","x","0","x","0","x","x","x","x"];
     const renderBoard=()=>{
         const htmlboard=document.querySelectorAll('.location')
         for(let i=0;i<9;i++){
@@ -8,7 +8,15 @@ const gameBoard=(()=>{
     }
 
     }
-    const checkWinner=()=>console.log("checking...");
+    const checkWinner=()=>{
+        
+        if((board[0]==board[1] && board[1]==board[2] &&board[1]!="")||(board[3]==board[4] && board[5]==board[4] && board[3]!="")||
+        (board[6]==board[7] && board[7]==board[8] && board[8]!="")) {
+            console.log("horizontal win")
+            return;
+        }
+    };
+
     const reset=()=>{
         for(let i=0;i<9;i++){
             board[i]=""
@@ -27,6 +35,7 @@ const gameBoard=(()=>{
 
 //initial board rendering
 gameBoard.renderBoard()
+gameBoard.checkWinner();
 
 //reset button to clear board
 document.querySelector(".reset").addEventListener('click',()=>{gameBoard.reset(); gameBoard.renderBoard();
